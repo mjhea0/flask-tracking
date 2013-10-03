5b0987ba9d74e62e940edb3b035f38fc9f482844
Sean Vieira <vieira.sean@gmail.com>
2013-10-02 23:48:44 -0400

    Resetting repository

--- part_2/app/tracking/forms.py
+++
@@ -1,7 +0,0 @@
-from flask.ext.wtf import Form, TextField, PasswordField, BooleanField, RecaptchaField, fields, validators
-from flask.ext.wtf import Required, Email, EqualTo
-from app.users.models import User
-from app import db
-
-class RegisterSiteForm(Form):
-    base_url = fields.TextField(validators=[validators.required()])
